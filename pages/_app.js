df37b57c264d4d880b9aa16a09cfc58296db8daa
Abner Freitas <defreitasabner@gmail.com>
2024-04-12 20:33:36 -0300

    Alterando algumas configurações do projeto.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
+
 import styles from '../src/styles/styles.css';
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <meta charset="UTF-8" />
+                <meta charSet="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Pet Happy</title>
             </Head>
