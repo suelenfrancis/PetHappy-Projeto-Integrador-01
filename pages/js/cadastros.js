874d6f11bc83c5b551f33b28c1d151cfc1c39785
Herbert Campos <herbertaudiovideo@gmail.com>
2024-04-25 21:37:25 -0300

    correção nomes id cadastro cliente. tem bug na validação do formulário

--- pages/js/cadastros.js
+++ pages/js/cadastros.js
@@ -96,108 +96,108 @@ $(function() {
      
   });
 
-  $(function() {
+  $(function () {
 
-    $("#dsNomeCompletoCliente").on('change', function() {		
-      if ($("#dsNomeCompletoCliente").val() === ''){
-        document.getElementById('dsNomeCompletoCliente').style.border='thin solid red';
+    $("#dsNomeCompleto").on('change', function() {		
+      if ($("#dsNomeCompleto").val() === ''){
+        document.getElementById('dsNomeCompleto').style.border='thin solid red';
         alert("O Campo do Nome não pode estar vazio!");
       }
       else{
-        document.getElementById('dsNomeCompletoCliente').style.border='thin solid green';
+        document.getElementById('dsNomeCompleto').style.border='thin solid green';
       }
     });
     
-    $("#nuTelefoneCliente").on('change', function() {		
-      if ($("#nuTelefoneCliente").val() === ''){
-        document.getElementById('nuTelefoneCliente').style.border='thin solid red';
+    $("#nuTelefone").on('change', function() {		
+      if ($("#nuTelefone").val() === ''){
+        document.getElementById('nuTelefone').style.border='thin solid red';
         alert("O Campo do Telefone não pode estar vazio!");
       }
       else{
-        document.getElementById('nuTelefoneCliente').style.border='thin solid green';
+        document.getElementById('nuTelefone').style.border='thin solid green';
       }
     });
     
-    $("#nuTelefoneEmergenciaCliente").on('change', function() {		
-      if ($("#nuTelefoneEmergenciaCliente").val() === ''){
-        document.getElementById('nuTelefoneEmergenciaCliente').style.border='thin solid red';
+    $("#nuTelefoneEmergencia").on('change', function() {		
+      if ($("#nuTelefoneEmergencia").val() === ''){
+        document.getElementById('nuTelefoneEmergencia').style.border='thin solid red';
         alert("O Campo do Telefone não pode estar vazio!");
       }
       else{
-        document.getElementById('nuTelefoneEmergenciaCliente').style.border='thin solid green';
+        document.getElementById('nuTelefoneEmergencia').style.border='thin solid green';
       }
     });
     
-    $("#dtNascimentoCliente").on('change', function() {		
-      if ($("#dtNascimentoCliente").val() === ''){
-        document.getElementById('dtNascimentoCliente').style.border='thin solid red';
+    $("#dtNascimento").on('change', function() {		
+      if ($("#dtNascimento").val() === ''){
+        document.getElementById('dtNascimento').style.border='thin solid red';
         alert("O Campo da Data de Nascimento não pode estar vazio!");
       }
       else{
-        document.getElementById('dtNascimentoCliente').style.border='thin solid green';
+        document.getElementById('dtNascimento').style.border='thin solid green';
       }
     });
     
-    $("#dsEstadoCliente").on('change', function() {		
-      if ($("#dsEstadoCliente").val() === ''){
-        document.getElementById('dsEstadoCliente').style.border='thin solid red';
+    $("#dsEstado").on('change', function() {		
+      if ($("#dsEstado").val() === ''){
+        document.getElementById('dsEstado').style.border='thin solid red';
         alert("O Campo do Estado não pode estar vazio!");
       }
       else{
-        document.getElementById('dsEstadoCliente').style.border='thin solid green';
+        document.getElementById('dsEstado').style.border='thin solid green';
       }
     });
     
-    $("#dsCidadeCliente").on('change', function() {		
-      if ($("#dsCidadeCliente").val() === ''){
-        document.getElementById('dsCidadeCliente').style.border='thin solid red';
+    $("#dsCidade").on('change', function() {		
+      if ($("#dsCidade").val() === ''){
+        document.getElementById('dsCidade').style.border='thin solid red';
         alert("O Campo da Cidade não pode estar vazio!");
       }
       else{
-        document.getElementById('dsCidadeCliente').style.border='thin solid green';
+        document.getElementById('dsCidade').style.border='thin solid green';
       }
     });
     
-    $("#dsEnderecoCliente").on('change', function() {		
-      if ($("#dsEnderecoCliente").val() === ''){
-        document.getElementById('dsEnderecoCliente').style.border='thin solid red';
+    $("#dsEndereco").on('change', function() {		
+      if ($("#dsEndereco").val() === ''){
+        document.getElementById('dsEndereco').style.border='thin solid red';
         alert("O Campo de Endereço não pode estar vazio!");
       }
       else{
-        document.getElementById('dsEnderecoCliente').style.border='thin solid green';
+        document.getElementById('dsEndereco').style.border='thin solid green';
       }
     });
     
-    $("#nuEnderecoCliente").on('change', function() {		
-      if ($("#nuEnderecoCliente").val() === ''){
-        document.getElementById('nuEnderecoCliente').style.border='thin solid red';
+    $("#nuEndereco").on('change', function() {		
+      if ($("#nuEndereco").val() === ''){
+        document.getElementById('nuEndereco').style.border='thin solid red';
         alert("O Campo de Endereço não pode estar vazio!");
       }
       else{
-        document.getElementById('nuEnderecoCliente').style.border='thin solid green';
+        document.getElementById('nuEndereco').style.border='thin solid green';
       }
     });
     
-    $("#dsEnderecoComplementoCliente").on('change', function() {		
-      if ($("#dsEnderecoComplementoCliente").val() === ''){
-        document.getElementById('dsEnderecoComplementoCliente').style.border='thin solid red';
+    $("#dsEnderecoComplemento").on('change', function() {		
+      if ($("#dsEnderecoComplemento").val() === ''){
+        document.getElementById('dsEnderecoComplemento').style.border='thin solid red';
         alert("O Campo de Endereço não pode estar vazio!");
       }
       else{
-        document.getElementById('dsEnderecoComplementoCliente').style.border='thin solid green';
+        document.getElementById('dsEnderecoComplemento').style.border='thin solid green';
       }
     });
     
-    $("#btnSalvarCadCliente").on('click', function() {
-    if($("#dsNomeCompletoCliente").val() != ''){
-      if($("#nuTelefoneCliente").val() != ''){
-        if($("#nuTelefoneEmergenciaCliente").val() != ''){
-          if($("#dtNascimentoCliente").val() != ''){
-            if($("#dsEstadoCliente").val() != ''){
-              if($("#dsCidadeCliente").val() != ''){
-                if($("#dsEnderecoCliente").val() != ''){
-                  if($("#nuEnderecoCliente").val() != ''){
-                    if($("#dsEnderecoComplementoCliente").val() != ''){
+    $("#btnSalvarCadCliente").on('click',function() {
+    if($("#dsNomeCompleto").val() != ''){
+      if($("#nuTelefone").val() != ''){
+        if($("#nuTelefoneEmergencia").val() != ''){
+          if($("#dtNascimento").val() != ''){
+            if($("#dsEstado").val() != ''){
+              if($("#dsCidade").val() != ''){
+                if($("#dsEndereco").val() != ''){
+                  if($("#nuEndereco").val() != ''){
+                    if($("#dsEnderecoComplemento").val() != ''){
                       alert("Cadastro realizado com sucesso!");
                     }
                     else{
