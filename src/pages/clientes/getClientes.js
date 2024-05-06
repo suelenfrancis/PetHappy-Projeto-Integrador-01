5993d754ece6c840cf48d9078433cb895693ae71
Abner Freitas <defreitasabner@gmail.com>
2024-05-06 18:20:21 -0300

    Implementada tela de visualização de clientes e funcionários.

---
+++ src/pages/clientes/getClientes.js
@@ -0,0 +1,34 @@
+import HttpClient from '../../middleware/HttpClient.js';
+
+
+const renderizaClientes = (clientes) => {
+    const tabela = document.querySelector('[data-clientes]');
+    const clientesHtml = clientes.map(
+        cliente => 
+        `
+        <div class="cartao">
+            <p class="cartao__nome">${cliente.pessoa.nome}</p>
+            <address>${cliente.pessoa.endereco.rua}, ${cliente.pessoa.endereco.numero} (${cliente.pessoa.endereco.bairro})</address>
+            <p>${cliente.pessoa.telefones.map(telefone => telefone.numero).join(',')}</p>
+        </div>
+        `
+    );
+    tabela.innerHTML = clientesHtml;
+};
+
+const getClientes = async () => {
+    try {
+        const response = await HttpClient.request(
+            'http://localhost:8000/clientes/',
+            'GET'
+        );
+        if (response.status == 200) {
+            console.log(response.body);
+            renderizaClientes(response.body);
+        }
+    } catch (error) {
+        alert(`Ocorreu um erro: ${error.message}`);
+    }
+};
+
+getClientes();
