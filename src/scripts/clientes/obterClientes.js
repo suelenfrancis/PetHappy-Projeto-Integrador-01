cb070966b697663033cd43acaebaceec776e1e1d
Abner Freitas <defreitasabner@gmail.com>
2024-05-22 19:54:41 -0300

    Últimos ajustes para o vídeo do Projeto Integrador 1.

--- src/scripts/clientes/obterClientes.js
+++ src/scripts/clientes/obterClientes.js
@@ -2,33 +2,33 @@ import HttpClient from '../../middleware/HttpClient.js';
 
 
 const renderizaClientes = (clientes) => {
-    const tabela = document.querySelector('[data-clientes]');
+    const clientesContainer = document.querySelector('[data-clientes]');
     const clientesHtml = clientes.map(
         cliente => 
         `
-        <div class="cartao">
+        <div class="cartao" data-id=${cliente.id}>
             <p class="cartao__nome">${cliente.pessoa.nome}</p>
             <address>${cliente.pessoa.endereco.rua}, ${cliente.pessoa.endereco.numero} (${cliente.pessoa.endereco.bairro})</address>
             <p>${cliente.pessoa.telefones.map(telefone => telefone.numero).join(',')}</p>
         </div>
         `
     );
-    tabela.innerHTML = clientesHtml.join(' ');
+    clientesContainer.innerHTML = clientesHtml.join(' ');
 };
 
-const getClientes = async () => {
+const obterClientes = async () => {
     try {
         const response = await HttpClient.request(
             'http://localhost:8000/clientes/',
             'GET'
         );
         if (response.status == 200) {
-            console.log(response.body);
-            renderizaClientes(response.body);
+            const clientes = response.body;
+            renderizaClientes(clientes);
         }
     } catch (error) {
         alert(`Ocorreu um erro: ${error.message}`);
     }
-};
+}
 
-getClientes();
+obterClientes();
