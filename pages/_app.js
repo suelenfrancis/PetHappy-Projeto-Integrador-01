924f9bcf6227161be41285aef77ea6ca58b66584
Abner Freitas <defreitasabner@gmail.com>
2024-07-25 17:06:19 -0300

    Corrigido warning sobre <title> no lugar errado.

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,17 @@ import '../src/styles/reset.css';
 import '../src/styles/global.css';
 import PageHeader from '../src/components/PageHeader/PageHeader';
 import PageFooter from '../src/components/PageFooter/PageFooter';
+import Head from 'next/head';
 
 
 
 function App({ Component, pageProps }) {
     return (
         <PrimeReactProvider>
+            <Head>
+                <title>Pet Happy</title>
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+            </Head>
             <PageHeader />
                 <Component { ...pageProps } />
             <PageFooter />
--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 function Document() {
     return (
         <Html lang='pt-br'>
-            <Head>
-                <title>Pet Happy</title>
-                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
-            </Head>
+            <Head/>
             <body>
                 <Main />
                 <NextScript />
