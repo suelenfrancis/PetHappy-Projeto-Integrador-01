a53a36ba220d54b8cc6ca0ba4b6ae2953fb6dd68
Abner Freitas <defreitasabner@gmail.com>
2024-10-28 15:37:31 -0300

    Cabeçalho ajustado e estilizado.

--- pet-happy/src/app/modules/shared/header/header.component.ts
+++ pet-happy/src/app/modules/shared/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { BehaviorSubject, filter, map, Observable, of, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  
+  public paginaAtual: string = '';
 
+  constructor(private router: Router) {}
+  
+  ngOnInit(): void {
+    this.paginaAtual = this.router.url.replace('/', '');
+  }
+
+  public selecionado(item: string) {
+    return this.paginaAtual === item
+      ? {'color': 'var(--selected-menu-item)'}
+      : {};
+  }
 }
